e191eb7e1b4f492b1f6cc9f652b52b3f938fec93
shumatsuura <shunsuke.mtr@gmail.com>
2020-08-10 20:26:47 +0900

    menu bar

--- app/models/pre_application.rb
+++ app/models/pre_application.rb
@@ -3,6 +3,5 @@ class PreApplication < ApplicationRecord
   has_many :approvals
   accepts_nested_attributes_for :approvals, allow_destroy: true
   has_many :reports
-
   has_many_attached :attached_files
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+# 30.times do
+#   user = User.create(name: Faker::Name.first_name,
+#                email: Faker::Internet.email,
+#                password: "password",
+#                password_confirmation: "password",
+#                department: ["管理","事業推進","事業企画"].sample,
+#                team: ["医療","セールス","開発","マーケティング"].sample,
+#                office: ["東京","名古屋"].sample,
+#                  )
+# end
 30.times do
-  user = User.create(name: Faker::Name.first_name,
-               email: Faker::Internet.email,
-               password: "password",
-               password_confirmation: "password",
-               department: ["管理","事業推進","事業企画"].sample,
-               team: ["医療","セールス","開発","マーケティング"].sample,
-               office: ["東京","名古屋"].sample,
-                 )
-   3.times do
-     a = user.pre_applications.create(
+  a = User.find_by(id: User.ids.sample(1)).pre_applications.create(
       genre:["基本","契約","財務","人事"].sample,
       item:[
       "押印（社長印）",
@@ -28,13 +21,6 @@
       "プロジェクト予算執行",
     ].sample,
       description: Faker::Lorem.sentence,
-      amount: (3000..1000000).to_a.sample,
-     )
-   end
-end
-
-pre_applications = PreApplication.all.sample(30)
-
-pre_applications.each do |pre_application|
-  pre_application.approvals.create(user_id: rand(1..30))
+      amount: (3000..1000000).to_a.sample,)
+  a.approvals.create(user_id: User.ids.sample(1).first)
 end
